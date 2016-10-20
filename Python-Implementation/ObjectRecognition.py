dc5c6ed8d4165a68c229a614de6aef8e4b190bda
Gobind03 <Gobind03@users.noreply.github.com>
2016-10-20 10:17:09 +0530

    Minor Improvements in Library

--- Python-Implementation/ObjectRecognition.py
+++ Python-Implementation/ObjectRecognition.py
@@ -2,32 +2,29 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def referTemplate(iFrame):
+def referTemplate(iFrame, threshold):
     
-    img_rgb = iFrame
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(iFrame, cv2.COLOR_BGR2GRAY)
 
     template = cv2.imread('train.jpg',0)
     w, h = template.shape[::-1]
 
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.75
     loc = np.where( res >= threshold)
 
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+        cv2.rectangle(iFrame, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
 
-    return img_rgb
+    return iFrame
 
 def bruteForceMatcher(iFrame):
     
-    img2 = iFrame
     img1 = cv2.imread('train.jpg',0)
 
     orb = cv2.ORB_create()
 
     kp1, des1 = orb.detectAndCompute(img1,None)
-    kp2, des2 = orb.detectAndCompute(img2,None)
+    kp2, des2 = orb.detectAndCompute(iFrame,None)
 
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
 
@@ -37,7 +34,7 @@ def bruteForceMatcher(iFrame):
     except MatchErrorException as e:
         print "No or Improbable Match Found"
 
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None, flags=2)
+    img3 = cv2.drawMatches(img1,kp1,iFrame,kp2,matches[:10],None, flags=2)
     
     #plt.imshow(img3)
     #plt.show()
