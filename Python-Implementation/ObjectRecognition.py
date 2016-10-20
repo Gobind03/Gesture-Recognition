6fae3ece39e3ba1496e8f32474fc581d9676edbe
Gobind03 <Gobind03@users.noreply.github.com>
2016-10-20 10:13:03 +0530

    Added Function for Brute Force Match
    Added method called bruteForceMatcher for comparing images with brute force match algorithm

--- Python-Implementation/ObjectRecognition.py
+++ Python-Implementation/ObjectRecognition.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def referTemplate(iFrame):
     
@@ -18,5 +18,30 @@ def referTemplate(iFrame):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
 
     return img_rgb
+
+def bruteForceMatcher(iFrame):
+    
+    img2 = iFrame
+    img1 = cv2.imread('train.jpg',0)
+
+    orb = cv2.ORB_create()
+
+    kp1, des1 = orb.detectAndCompute(img1,None)
+    kp2, des2 = orb.detectAndCompute(img2,None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
+    try:
+        matches = bf.match(des1,des2)
+        matches = sorted(matches, key = lambda x:x.distance)
+    except MatchErrorException as e:
+        print "No or Improbable Match Found"
+
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None, flags=2)
+    
+    #plt.imshow(img3)
+    #plt.show()
+    
+    return img3
 
 
